4eb1ff87f2d68e4b0515053ce291b0b9016eaa41
kaivalyavohra <kaivalyavohra@gmail.com>
2018-10-18 20:03:27 +0400

    Delete loginfunc.py

--- loginfunc.py
+++
@@ -1,7 +0,0 @@
-from faces import detect
-
-name=input("Welcome to this program. Please enter your first name if you would like to log in: ").lower()
-if detect().lower()!=name:
-	print("Unsuccesful attempt.")
-else:
-	print("Succesfully logged in.")
